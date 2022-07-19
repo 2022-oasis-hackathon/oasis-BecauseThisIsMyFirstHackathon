eb8623a25c62e3daa68edb1ba97130a89bb9610d
nhy0052 <nhy0052@naver.com>
2022-07-19 22:37:59 +0900

    journal seeDetails

--- HY/public/js/journal_list.js
+++ HY/public/js/journal_list.js
@@ -18,7 +18,8 @@ function showList(exDatas) {
     const newTD2 = document.createElement('td');
     newTD2.innerHTML = exDatas.result[i].journalDate.replaceAll('-','.');
     newTD2.classList.add("date");
-    newTD2.id = exDatas.result[i].id;
+    newTD2.id = exDatas.result[i].id; 
+    newTD2.addEventListener("click",event => showEach(exDatas, newTD2.id));       
 
     newTR.appendChild(newTD1);
     newTR.appendChild(newTD2);
@@ -29,6 +30,32 @@ function showList(exDatas) {
 }
 
 
+// 아이디 인자로 받아서 세부사항 띄우는 함수
+function showEach(ExDatas, ID){
+  console.log(ID);
+  ID = Number(ID);
+  let temp;
+  for(let i=0;i<ExDatas.result.length;i++){
+    if(ID===ExDatas.result[i].id){
+      temp = ExDatas.result[i];
+      break;
+    }
+  }
+  console.log(temp);
+
+  document.querySelector("#jDate").innerHTML = temp.journalDate.replaceAll('-','.');
+  document.querySelector("#jTime").innerHTML = `${temp.journalHours}시간`;
+
+  let content = temp.journalEtc;
+  content = content.replace("<p>",'');
+  content = content.replace("</p>",'');
+  content = content.replaceAll("<br />",'\n');
+
+  document.querySelector("#jEtc").innerText = content;
+
+
+
+}
 
 
 fetch(`http://localhost:3000/api/journal/record`)       // 쿼리스트링으로 요청 보내기
@@ -37,9 +64,11 @@ fetch(`http://localhost:3000/api/journal/record`)       // 쿼리스트링으로
   Datas = JSON.parse(result);
   // console.log(result);
   // console.log(Datas);
-  // console.log(Datas.result.length);
 
   showList(Datas);
   
 
 });
+
+
+
